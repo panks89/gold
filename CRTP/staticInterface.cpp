43ff66085f7eaf0c58f0674d6cd1215525126821
pankaj kuamr <pankaj@localmachine>
2021-05-09 21:42:15 +0200

    C++20 Coroutines - Future

--- CRTP/staticInterface.cpp
+++ CRTP/staticInterface.cpp
@@ -4,44 +4,46 @@
 
 template<typename T, template<typename> class crtpType>
 class crtp {
-  crtp() {}
-  friend crtpType<T>;
+    crtp() {}
+    friend crtpType<T>;
 public:
-  T& underlying() { return static_cast<T&>(*this); }
-  const T& underlying() const { return static_cast<const T&>(*this); }
+    T& underlying() { return static_cast<T&>(*this); }
+    const T& underlying() const { return static_cast<const T&>(*this); }
 };
 
 template<typename T>
 class doubler : public crtp<T, doubler> {
 public:
-  void doubleIt() {
-    this->underlying().setValue(2 * this->underlying().getValue());
-  }
+    using CRTP = doubler;
+    void doubleIt() {
+        this->underlying().setValue(2 * this->underlying().getValue());
+    }
 };
 
 template<typename T>
 class half : public crtp<T, half> {
 public:
-  void halfIt() {
-    this->underlying().setValue(this->underlying().getValue()/2);
-  }
+    void halfIt() {
+        this->underlying().setValue(this->underlying().getValue()/2);
+    }
 };
 
 
-class data : public doubler<data>, public half<data> {
-  int value;
+class data : public half<data>, private doubler<data> {
+    int value;
 public:
-  /* Getter and Setter */
-  const int& getValue() { return value; }
-  void setValue(int v) { value = v; }
+    friend CRTP;
+    /* Getter and Setter */
+    const int& getValue() { return value; }
+    void setValue(int v) { value = v; }
 };
 
 int main() {
-  data d;
-  d.setValue(10);
-  std::cout << " Current data value : " << d.getValue() << std::endl;
-  d.doubleIt();
-  std::cout << " New data value : " << d.getValue() << std::endl;
-  d.halfIt();
-  std::cout << " New data value : " << d.getValue() << std::endl;
+    data d;
+    d.setValue(10);
+    std::cout << " Current data value : " << d.getValue() << std::endl;
+    d.doubleIt();
+    std::cout << " New data value : " << d.getValue() << std::endl;
+    d.halfIt();
+    std::cout << " New data value : " << d.getValue() << std::endl;
 }
